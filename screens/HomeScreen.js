b18d4e880fd9f931929c231ce8ca0da61ef4037e
rajchatterz <iamraj.space@gmail.com>
2024-01-03 15:35:53 +0530

    done with the onboarding ui

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import LottieView from 'lottie-react-native'
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+          <LottieView style={{ width: 300, height: 600 }} source={require('../assets/aa.json')} autoPlay loop />
+          <Text style={{fontSize:30}}>Home Page</Text>
     </View>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
